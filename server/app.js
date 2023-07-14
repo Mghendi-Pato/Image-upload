26d52bd72296a972a0e603ab896d91b8488175d5
Mghendi-Pato <mwamburipatrick8@gmail.com>
2023-07-14 19:37:16 +0300

    Added delete image functionality

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ app.listen(PORT, () => {
   console.log(`listening to port: ${PORT}`);
 });
 
+//Upload image to cloudinary
 app.post("/upload", async (req, res) => {
   try {
     const file = req.body.data;
@@ -21,5 +22,33 @@ app.post("/upload", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(400).json({ message: error.message });
+  }
+});
+
+//Get images from cloudinary
+app.get("/images", async (req, res) => {
+  try {
+    const { resources } = await cloudinary.search
+      .expression("folder:Test")
+      .sort_by("public_id", "desc")
+      .max_results(30)
+      .execute();
+    const publicIds = resources.map((file) => file.public_id);
+    res.send(publicIds);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ message: "Error retrieving images", error });
+  }
+});
+
+//Delete image
+app.delete("/images", async (req, res) => {
+  const { image } = req.body;
+  try {
+    await cloudinary.uploader.destroy(image);
+    // Respond with a success message
+    res.json({ message: "Image deleted successfully" });
+  } catch (error) {
+    res.json({ message: "Error deleting image" });
   }
 });
--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./base.css";
+import { Image } from "cloudinary-react";
+
 function App() {
   //Set state of the image
   const [selectedFile, setSelectedFile] = useState("");
@@ -24,6 +26,7 @@ function App() {
     e.preventDefault();
     if (!selectedFile) return;
     uploadImage(selectedFile);
+    loadImages();
   };
 
   //Handle image upload on submission
@@ -39,6 +42,36 @@ function App() {
     }
   };
 
+  //MANAGE IMAGGE VIEWING
+  const [imageIds, setImageIds] = useState([]);
+
+  const loadImages = async () => {
+    try {
+      const res = await fetch("/images");
+      const data = await res.json();
+      setImageIds(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const deleteImage = async (image) => {
+    console.log(JSON.stringify(image));
+    try {
+      const res = await fetch("/images", {
+        method: "DELETE",
+        body: JSON.stringify({ image }),
+        headers: { "content-type": "application/json" },
+      });
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    loadImages();
+  }, []);
   return (
     <>
       <div style={{ margin: "10px" }}>
@@ -55,8 +88,44 @@ function App() {
           </button>
         </form>
         {selectedFile && (
-          <img src={selectedFile} style={{ height: "300px" }} alt="selected" />
+          <img src={selectedFile} style={{ height: "100px" }} alt="selected" />
         )}
+        <hr />
+
+        {/* VIEW IMAGES */}
+        <div
+          style={{ margin: "10px", display: "flex", flexDirection: "column" }}
+        >
+          <h1 className="title">Uploaded images</h1>
+          {imageIds &&
+            imageIds.map((imageId, index) => (
+              <div
+                key={index}
+                style={{ display: "flex", flexDirection: "row" }}
+              >
+                <Image
+                  style={{ margin: "5px" }}
+                  cloudName="dodxjpwfu"
+                  publicId={imageId}
+                  width="150"
+                  crop="scale"
+                />
+                <button
+                  className="btn"
+                  style={{
+                    margin: "5px",
+                    padding: "5px",
+                    background: "red",
+                    height: "30px",
+                    fontSize: "9px",
+                  }}
+                  onClick={() => deleteImage(imageId)}
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+        </div>
       </div>
     </>
   );
